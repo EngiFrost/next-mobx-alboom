0f140bb495a3b7b6922487bbae166abdaa6eb6e9
EngiFrost <engifrost@gmail.com>
2022-09-16 21:39:22 +0300

    created album page

---
+++ pages/albums/[albumId].tsx
@@ -0,0 +1,28 @@
+import { GetServerSideProps, NextPage } from 'next';
+import MainLayout from '../../layouts/MainLayout';
+import { Album } from '../../types/album';
+
+type AlbumPageProps = {
+  album: Album;
+};
+
+const Album: NextPage<AlbumPageProps> = ({ album }) => {
+  return (
+    <MainLayout title="Album page!">
+      <div>Album title: {album.title}</div>
+    </MainLayout>
+  );
+};
+
+export default Album;
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const album: Album = await fetch(`https://jsonplaceholder.typicode.com/albums/${params.albumId}`).then((res) => res.json());
+  return album
+    ? {
+        props: { album },
+      }
+    : {
+        notFound: true,
+      };
+};
